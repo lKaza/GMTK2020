e019219d63f99b36f52314bedc9f6fd29a2f6d96
lKaza <rodrigoguzman1202@gmail.com>
2020-07-10 17:02:45 -0400

    Procedural/Platform Generation

---
+++ Assets/PlatformGeneration.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    [SerializeField] int width,height;
+    [SerializeField] int minHeight,maxHeight;
+    [SerializeField] GameObject dirt, grass;
+    [SerializeField] int repeatNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generation();
+    }
+
+    private void Generation()
+    {
+        int repeatValue = 0;
+        for (int x = 0; x < width; x++)
+        {
+            if(repeatValue == 0)
+            {
+                GetRandomHeight();
+                GenerateFlatPlatform(x);
+                repeatValue = repeatNum;
+
+            }else{
+                GenerateFlatPlatform(x);
+                repeatValue--;
+            }
+            
+        }
+    }
+
+    void GenerateFlatPlatform(int x)
+    {
+        for (int y = 0; y < height; y++)
+        {
+
+            spawnObj(dirt, x, y);
+
+        }
+        spawnObj(grass, x, height);
+    }
+    private void GetRandomHeight()
+    {
+       
+        height = UnityEngine.Random.Range(minHeight, maxHeight);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void spawnObj(GameObject obj, int width, int height)
+    {
+        obj = Instantiate(obj, new Vector2(width, height), Quaternion.identity);
+        obj.transform.parent = this.transform;
+       
+    }
+}
