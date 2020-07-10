40831e5e88bb9f5f148a273f4581525bf25a94bb
lKaza <rodrigoguzman1202@gmail.com>
2020-07-10 15:39:53 -0400

    2019.4.3f1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]  float velocity=1f;
+    [SerializeField] float acceleration = 1f;
+    Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    private void Awake() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement,0,0) * Time.deltaTime * velocity;
+    }
+}
