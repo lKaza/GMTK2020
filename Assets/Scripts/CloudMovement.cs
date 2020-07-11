613fed32d4bc2720190cfcc51c28856b1d84b1db
ZeroHex <luis.tapia.personal@gmail.com>
2020-07-11 16:32:03 -0400

    Eliminación del script top down, creado sistema de animación de nubes

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+
+    //Marco de generación
+    [SerializeField] public Vector2 boxcloudpos = new Vector2(0,0);
+    
+    [SerializeField] public float height = 15;
+    [SerializeField] public float length = 10 ;
+    [SerializeField] public float rightdistance;
+    [SerializeField] public float leftdistance;
+    [SerializeField] public GameObject player;
+    float totalcloud;
+
+    public GameObject cloudcontainer;
+    public GameObject[] cloudcontainerarray;
+
+    [SerializeField] float cloudspeed;
+
+    Vector2 playerpos;
+    Vector2 cloudpos;
+
+    void Start()
+    {
+        cloudstart();
+    }
+
+    void Update()
+    {
+        
+        playerpos = player.transform.position;
+        boxcalculator();
+
+    }
+    void cloudstart()
+    {
+        cloudcontainer = GameObject.Find("cloudscontainer");
+        cloudspeed = 1;
+        cloudfinder();
+        leftdistance = 0;
+        rightdistance = length;
+    }
+    
+    //
+    void boxcalculator() {
+        if (playerpos.x >= (rightdistance - 1))
+        {
+            boxcloudpos.x = boxcloudpos.x + 1;
+            leftdistance = rightdistance;
+            rightdistance = length * boxcloudpos.x;
+            generatecloud();
+        }
+    }
+    void cloudfinder()
+    {
+        totalcloud = 0;
+        cloudcontainerarray = GameObject.FindGameObjectsWithTag("BackgroundCloud");
+        for(int i = 0; i < cloudcontainerarray.Length; i++)
+        {
+            cloudcontainerarray[i].gameObject.transform.parent = cloudcontainer.transform;
+            cloudcontainerarray[i].gameObject.SetActive(true);
+            cloudcontainerarray[i].gameObject.name = "CloudID:" + i;
+            totalcloud = i;
+        }
+    }
+    void generatecloud()
+    {
+        for(int i = 0; i <= totalcloud; i++)
+        {
+            if(cloudcontainerarray[i].gameObject.transform.position.x < leftdistance)
+            {
+                transform.position = new Vector2(cloudcontainerarray[i].gameObject.transform.position.x + length, 0);
+            }
+        }
+    }
+
+
+    
+}
