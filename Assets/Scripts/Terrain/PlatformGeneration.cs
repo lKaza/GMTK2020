edf2103c92c22fdb62f43ddd9eb1188a1ecb4b94
lKaza <rodrigoguzman1202@gmail.com>
2020-07-11 02:57:03 -0400

    Procedural para ambos lados

--- Assets/Scripts/Terrain/PlatformGeneration.cs
+++ Assets/Scripts/Terrain/PlatformGeneration.cs
@@ -4,64 +4,101 @@ using UnityEngine;
 
 public class PlatformGeneration : MonoBehaviour
 {
-    [SerializeField] int width,height;
+    [SerializeField] int width;
     [SerializeField] int minHeight,maxHeight;
     [SerializeField] GameObject dirt, grass,crate,crate2;
     [SerializeField] int repeatNum;
-    
+    private int height;
+    private int lastPositiveXPosition=0;
+    private int lastNegativeXPosition=0;
    
     int widthIncreaseSize =0;
     int currentStartPosition;
     ContactPoint2D[] contacts = new ContactPoint2D[10];
     // Start is called before the first frame update
     private void Awake() {
-        widthIncreaseSize = width;
+       
         currentStartPosition = width;
     }
     void Start()
     {
         
-        Generation(0);
+        Generation(0,true);
     }
 
-    public void Generation(int startPosition)
+    public void Generation(int startPosition,bool side)
     {
         int repeatValue = 0;
-        if(startPosition == currentStartPosition){
-            width = width+widthIncreaseSize;
-            currentStartPosition = currentStartPosition+widthIncreaseSize;
-        }
-        for (int x = startPosition; x < width; x++)
+        
+
+        TerrainLoop(startPosition, repeatValue,CheckSide(startPosition, side));
+    }
+
+    private int CheckSide(int startPosition, bool side)
+    {
+        if (side)
         {
-            
-           
-            if(repeatValue == 0)
+    
+            lastPositiveXPosition = lastPositiveXPosition + width;
+            return lastPositiveXPosition;
+        }
+        else
+        {
+
+            lastNegativeXPosition = lastNegativeXPosition - width;
+            return lastNegativeXPosition;
+        }
+        
+    }
+
+    private void TerrainLoop(int startPosition, int repeatValue,int lastPosition)
+    {
+        if(lastPosition>0){
+        for (int x = startPosition; x < lastPosition; x++)
+        { //si -120 < -150
+            if (repeatValue == 0)
             {
                 GetRandomHeight();
                 GenerateFlatPlatform(x);
                 repeatValue = repeatNum;
-
-            }else{
+            }
+            else
+            {
                 GenerateFlatPlatform(x);
                 repeatValue--;
             }
         }
+
+        }else{
+            for (int x = startPosition; x > lastPosition; x--)
+            {
+                if (repeatValue == 0)
+                {
+                    GetRandomHeight();
+                    GenerateFlatPlatform(x);
+                    repeatValue = repeatNum;
+                }
+                else
+                {
+                    GenerateFlatPlatform(x);
+                    repeatValue--;
+                }
+        }}
+    }
+
+    private void GetRandomHeight()
+    {
+       
+        height = UnityEngine.Random.Range(minHeight, maxHeight);
     }
 
     void GenerateFlatPlatform(int x)
     {
         for (int y = 0; y < height; y++)
         {
-
             spawnObj(dirt, x, y);
-
         }
         spawnObj(grass, x, height);
-    }
-    private void GetRandomHeight()
-    {
-       
-        height = UnityEngine.Random.Range(minHeight, maxHeight);
     }
     
 
@@ -82,7 +119,11 @@ public class PlatformGeneration : MonoBehaviour
 
     }
     
-    public int GetCurrentStartPos(){
-        return currentStartPosition;
+    public int GetCurrentStartPositivePos(){
+        return lastPositiveXPosition;
+    }
+    public int GetCurrentStartNegativePos()
+    {
+        return lastNegativeXPosition;
     }
 }
