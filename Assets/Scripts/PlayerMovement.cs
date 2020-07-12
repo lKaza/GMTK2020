becf05e6e0041b39f5b067c5fc9f6d62379ea38c
lKaza <rodrigoguzman1202@gmail.com>
2020-07-11 20:15:11 -0400

    cratebomb

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,49 +10,48 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float reduceVelocityOverTime = 2f;
     Rigidbody2D rigidBody;
     float deltaVelocity;
+    Health health;
     // Start is called before the first frame update
     private void Awake() {
         rigidBody = GetComponent<Rigidbody2D>();
+        health = GetComponent<Health>();
     }
     void Start()
     {
+        
         //StartCoroutine(ReduceAngularVelocity(reduceVelocityOverTime));
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
        PMovement();
-
-
     }
+
     void PMovement()
     {
         
        Quaternion rot = transform.rotation;
        float z = rot.eulerAngles.z;
-
-       z -= Input.GetAxis("Horizontal") * rotationVelocity * Time.deltaTime;
+        if(health.GetisDead()){
+            z=transform.rotation.eulerAngles.z;
+            SetVelocity(0);
+        }else{
+            z -= Input.GetAxis("Horizontal") * rotationVelocity * Time.deltaTime;
+        }
+       
        rot = Quaternion.Euler(0,0,z);
        transform.rotation = rot;
 
        Vector3 pos = transform.position;
        rigidBody.AddForce(transform.up* rocketVelocity * Time.deltaTime,ForceMode2D.Force);
-       
+
        transform.position = pos;
-
-
     }
-    IEnumerator ReduceAngularVelocity(float transitionTime)
-    {
-  
-        while (rigidBody.angularVelocity > 1)
-        {
-            deltaVelocity = Time.deltaTime / transitionTime;
-            rigidBody.angularVelocity -= deltaVelocity;
-            yield return transitionTime;
-        }
-        
+
+
+    public void SetVelocity(int amount){
+        rigidBody.gravityScale = 3;
+        rocketVelocity = amount;
     }
 }
