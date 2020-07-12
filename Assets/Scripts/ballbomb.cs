7dd7d0138e3ac53f243ef5d53653ea4d72203ee9
ZeroHex <luis.tapia.personal@gmail.com>
2020-07-11 22:24:20 -0400

    fixed

--- Assets/Scripts/ballbomb.cs
+++ Assets/Scripts/ballbomb.cs
@@ -21,16 +21,4 @@ public class ballbomb : MonoBehaviour
     {
         
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-<<<<<<< HEAD
-       
-
-        //como eso?
-        //
-=======
-        
->>>>>>> d96ba3eb0d2d29308359396e3401960e10dafc40
-    }
 }
